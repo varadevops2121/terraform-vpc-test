0fce3ccf948c808d8f52f1e79496598a20497853
varalaxmi <varadevops2121@gmail.com>
2024-10-28 23:28:45 +0530

    ansible roles

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,15 @@
-# output "azs" {
-#     value = data.aws_availability_zones.available.names
+# output "azs_info" {
+#     value = module.vpc.azs # module.<module-name>.<output>
 # }
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
 
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
-
-output "database_subnet_group_id" {
-  value = aws_db_subnet_group.default.id
-}
-
-output "database_subnet_group_name" {
-  value = aws_db_subnet_group.default.name
+output "public_subnet_list" {
+  value = module.vpc.public_subnet_ids
 }
 
 output "igw_id" {
-  value = aws_internet_gateway.gw.id
+  value = module.vpc.igw_id
 }
--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,5 @@
 module "vpc" {
+     #source = "../terraform-aws-vpc"
     source = "git::https://github.com/varadevops2121/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     common_tags = var.common_tags
